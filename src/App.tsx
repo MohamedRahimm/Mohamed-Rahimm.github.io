a1cd2859d2db4138918f996513b4cbaa0198fd0a
Mohamed <javidrahim2005@gmail.com>
2024-10-07 05:06:37 -0400

    updated links, fixed minor bug

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ export default function App() {
           description="A Logistic Regression model implemented in NumPy that predicts weight categories based on BMI data, while also considering age and fitness levels. It uses L2 regularization, dropout and other techniques to prevent overfitting and optimize performance. " 
           date="April 2024 - May 2024"
           imgURL={new URL('./assets/weight_cat.png',import.meta.url).href}
-          websiteURL="https://github.com/MohamedRahimm/ml-from-scratch"
+          websiteURL="https://mohamedrahimm.github.io/ml-from-scratch/"
           sourceCodeURL="https://github.com/MohamedRahimm/ml-from-scratch"
         />
         <Project
@@ -42,7 +42,7 @@ export default function App() {
           description="An LSTM sentiment analysis model using PyTorch on the IMDB movie dataset.Extensive data preprocessing reduced training time with a custom tokenizer using Hugging Face to optimize vocabulary size." 
           date="June 2024 - July 2024"
           imgURL={new URL('./assets/senti_analysis.png',import.meta.url).href}
-          websiteURL="https://github.com/MohamedRahimm/torch-ML"
+          websiteURL="https://mohamedrahimm.github.io/ml-from-scratch/"
           sourceCodeURL="https://github.com/MohamedRahimm/torch-ML"
         />
 
